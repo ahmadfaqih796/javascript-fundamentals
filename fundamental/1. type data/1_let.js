2d4af41a6862d2686970762a8b9f4feb491cece4
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2026-05-27 12:59:06 +0700

    type data

---
+++ fundamental/1. type data/1_let.js
@@ -0,0 +1,12 @@
+// nilai yang bisa berubah
+let age = 25;
+age = 26;
+
+let counter = 0;
+counter++;
+
+console.log("age", age);
+console.log("counter 1", counter++);
+console.log("counter 2", counter++);
+console.log("counter 3", counter++ + counter++);
+console.log("counter 4", counter);
---
+++ fundamental/1. type data/2_const.js
@@ -0,0 +1,5 @@
+const name = "Faqih";
+// pasti bakal error karena nilai const tidak bisa diubah
+name = "Budi";
+
+console.log(name);
