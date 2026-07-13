923a6a407378a31ea7b2cb86b7628b4971dfec14
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2026-07-13 08:57:35 +0700

    number

---
+++ fundamental/2. type data/2_number.js
@@ -0,0 +1,9 @@
+const age = 25000;
+const price = 2.5;
+
+// Javascript tidak membedakan antara integer dan float
+console.log(age);
+console.log(typeof age); // number
+
+console.log(price);
+console.log(typeof price); // number
