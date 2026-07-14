06b5a23f245da40ebd5336e984e98cf95b8396f1
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2026-07-14 08:24:46 +0700

    operator aritmatika

---
+++ fundamental/3. operator & condition/1_operator_aritmatika.js
@@ -0,0 +1,20 @@
+const a = 10;
+const b = 5;
+
+console.log(a + b); // 15
+console.log(a - b); // 5
+console.log(a * b); // 50
+console.log(a / b); // 2
+console.log(a % b); // 0
+
+// modulo sisa pembagian
+// 10 ÷ 3 = 3 sisa 1
+console.log(10 % 3); // 1
+
+// pengecekan ganjil genap
+const angka = 8;
+if (angka % 2 === 0) {
+   console.log("genap");
+} else {
+   console.log("ganjil");
+}
